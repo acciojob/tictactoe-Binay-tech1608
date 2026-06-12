e66da47c6b6bee39140664da6ce97194459179e2
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-06-12 14:20:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+
+const mybtn = document.getElementById("btn");
+
+mybtn.addEventListener("click" , function(){
+	const element = document.createElement("h1");
+	element.innerHTML = "TIC TAC TOE";
+	element.style.fontStyle = "italic";
+	element.style.fontFamily = "cursive";
+
+	const boardcontainer = document.getElementById("game-board");
+	boardcontainer.innerHTML = '';
+
+	for(let i=0; i<9; i++){
+		const cell = document.createElement('div');
+		cell.classList.add('cell');
+		boardcontainer.appendChild(cell);
+	}
+});
